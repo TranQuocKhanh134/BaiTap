6c6bb349f208b6be2e1c82283bc0c3a11d4f95f6
Trần Quốc Khánh <S2khanh123@gmail.com>
2021-10-04 12:16:30 +0700

    Nộp Bài tập về nhà

---
+++ src/com/khanhtq2008110082/BankProject.java
@@ -0,0 +1,25 @@
+/*
+*Ngày tạo : 04 thg 10, 2021
+*Tác giả : Trần Quốc Khánh
+*/
+package com.khanhtq2008110082;
+
+public class BankProject {
+    public static void main(String[] args) {
+        Account account = new Account(123456, 200000);
+        account.Withdraw(20000);
+        account.deposite(20000000);
+        account.Show_Balance();
+        System.out.println("<==============================================>");
+        Customer account1 = new Customer();
+        account1.getName();
+        account1.getAddress();
+        account1.printInformation();
+        System.out.println("<==============================================>");
+        Emloyee account2 = new Emloyee();
+        account2.getName();
+        account2.getSalary();
+        account2.printInfomationEmployee();
+        System.out.println("<==============================================>");
+    }
+}
