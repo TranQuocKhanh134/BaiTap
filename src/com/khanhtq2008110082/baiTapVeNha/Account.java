fe33628e0f30e9145c5663c09b3f0ce15ae2ccc3
Trần Quốc Khánh <S2khanh123@gmail.com>
2021-10-04 12:24:41 +0700

    Nộp bài tập về nhà

---
+++ src/com/khanhtq2008110082/baiTapVeNha/Account.java
@@ -0,0 +1,24 @@
+/*
+*Ngày tạo : 04 thg 10, 2021
+*Tác giả : Trần Quốc Khánh
+*/
+package com.khanhtq2008110082.baiTapVeNha;
+
+public class Account {
+    int accout_number ;
+    int accout_balance ;
+    Account(int a , int b){
+        accout_number = a;
+        accout_balance = b;
+    }
+    public void Show_Balance() {
+        System.out.println("accout_number = " + accout_number);
+        System.out.println("accout_balance = " + accout_balance);
+    }
+    public void deposite(int ammout){
+        accout_balance += ammout;
+    }
+    public void Withdraw(int ammout){
+        accout_balance -= ammout;
+    }
+}
