1c10dac4ea3bd68817c574e3472c077d3021ad2d
Trần Quốc Khánh <S2khanh123@gmail.com>
2021-10-11 10:00:27 +0700

    nộp bài tuần 5

---
+++ src/com/khanhtq2008110082/BaiTapTuan5/cow.java
@@ -0,0 +1,20 @@
+/*
+*Ngày tạo : 11 thg 10, 2021
+*Tác giả : Trần Quốc Khánh
+*/
+package com.khanhtq2008110082.BaiTapTuan5;
+
+public class cow {
+    float weight;
+    int age;
+    cow(int tuoi , float canNang){
+        age = tuoi;
+        weight = canNang;
+    }
+    void keu(){
+        System.out.println(" MOO MOO MOO ");
+    }
+    void an(){
+        System.out.println(" Măm măm măm");
+    }
+}
