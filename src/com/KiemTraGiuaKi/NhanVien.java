01ecf500fbf441a9caaf5cbaba51385628787376
Trần Quốc Khánh <S2khanh123@gmail.com>
2021-11-04 08:48:26 +0700

    Nộp bài giữa kì

---
+++ src/com/KiemTraGiuaKi/NhanVien.java
@@ -0,0 +1,23 @@
+/*
+*Ngày tạo : 04 thg 11, 2021
+*Tác giả : Trần Quốc Khánh
+*/
+package com.KiemTraGiuaKi;
+
+public class NhanVien extends Nguoi{
+    private float heSoLuong;
+    public void setHeSoLuong(float heSoLuong){
+        this.heSoLuong = heSoLuong;
+    }
+    public double getHeSoLuong(){
+        return this.heSoLuong;
+    }
+    protected double tinhLuong(){
+        return (this.heSoLuong * 1500000);
+    }
+    public String toString(){
+        String s ; 
+        s = "\nTên nhân viên : "+getHoTen() + "\n" +"Địa chỉ : "+getDiaChi() +"\nHệ số lương nhân viên : " + getHeSoLuong() + "\n" +"Lương nhân viên : " + tinhLuong();
+        return s; 
+    }
+}
